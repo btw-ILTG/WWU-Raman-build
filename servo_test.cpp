17a0b3f1edab87f089bf031927bdc4ba31b1786f
feliciamartinez <53018870+feliciamartinez@users.noreply.github.com>
2019-12-13 15:36:49 -0800

    Add files via upload

---
+++ servo_test.cpp
@@ -0,0 +1,17 @@
+#include "mbed.h"
+
+PwmOut servo(PA_11);
+DigitalOut grnLED(LED1);
+
+int main() {
+    servo.period_ms(20);
+
+    while(1){
+        grnLED = 0;
+        servo.pulsewidth_us(600); //pulsewidth 600 is shutter open
+        wait(2);
+        grnLED = 1;
+        servo.pulsewidth_us(1500); //rotate 90 to close shutter
+        wait(2);
+    }
+}
