d012564bd31c3bbf8cceeed3784ba9260316a561
Stefan Hess <speterhess8@gmail.com>
2021-06-22 11:05:56 -0700

    Laser cmd with packet start and packet end

---
+++ raspberry pi files/raspi_nucleo_laserpower_test.cpp
@@ -0,0 +1,102 @@
+// Testing communication between raspberry pi and the nucleo board
+// With the laser power on
+#include "mbed.h"
+#include <cstdint>
+
+#define packet_start        0xF0
+#define packet_flag         0xF2
+#define packet_end          0xF3
+
+#define cmd_laser           0xA0
+#define laser_on       	    0xA1
+#define laser_off           0xA2
+#define shutter_open        0xA3
+#define shutter_close       0xA4
+#define laser_temp          0xA5
+
+static BufferedSerial raspi(D8, D2, 9600);
+
+const int usDelay = 1000;
+
+static DigitalOut myled(LED1);
+static DigitalOut relay(PA_1);
+
+void laserPower(int on){
+    if(on == 1){//turn laser on
+        relay = 0;
+        myled = 1;
+    }
+    if(on == 0){//turn laser off
+        relay = 1;
+        myled = 0;
+    }
+
+}
+
+int main(){
+    raspi.set_format(
+        /* bits */ 8,
+        /* parity */ BufferedSerial::None,
+        /* stop bit */ 1
+    );
+    // Initialize laser to off
+    laserPower(0);
+
+    int packetFlag = 0;
+
+    
+    while(true) {
+        char c;
+       
+        if (ssize_t num = raspi.read(&c, 1)) {
+            switch (c) {
+                case packet_start:
+                    raspi.write("Packet start", 12);
+                    if (ssize_t num = raspi.read(&c, 1)) { 
+                        switch(c) {
+                            case packet_flag:
+                                packetFlag = 1;
+                                raspi.write("Packet flag", 11);
+                                while (packetFlag == 1) {
+                                    if (ssize_t num = raspi.read(&c, 1)) {
+                                        switch (c) {
+                                            case cmd_laser:
+                                                raspi.write("Cmd laser", 9);
+                                                if (ssize_t num = raspi.read(&c, 1)) {
+                                                    switch(c) {  
+                                                        case laser_on:
+                                                            laserPower(1);
+                                                            raspi.write("Laser on", 8);
+                                                            continue;
+
+                                                        case laser_off:
+                                                            laserPower(0);
+                                                            raspi.write("Laser off", 9);
+                                                            continue;
+                                                        default:
+                                                            raspi.write("Laser cmd fail", 14);
+                                                    }
+                                                }
+                                                continue;
+                                            case packet_end:
+                                                packetFlag = 0;
+                                                raspi.write("Packet end\n", 11);
+                                                break;
+                                            default:
+                                                raspi.write("Not IMP", 7);
+                                        }
+                                    }
+                                }
+                                continue;
+                            default:
+                                break;
+                        }
+                    }                          
+                          
+                default:
+                    raspi.write("Different\n", 10);
+                    continue;
+            }
+        }
+    }
+}
