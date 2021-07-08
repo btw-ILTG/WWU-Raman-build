196e845652c434b31d4d657015f75a6e046c735a
Stefan Hess <speterhess8@gmail.com>
2021-07-08 15:23:22 -0700

    Added mbed_app.json

--- StefansDev/packetForge/serial-fun/main.cpp
+++ StefansDev/packetForge/serial-fun/main.cpp
@@ -7,9 +7,9 @@ FileHandle *mbed::mbed_override_console(int fd) {
 }
 
 char readChar(BufferedSerial *serialDevice) {
-    char c[4];
+    char c[3];
     while (true) {
-        if (ssize_t num = serialDevice->read(&c, 4)) {
+        if (ssize_t num = serialDevice->read(&c, sizeof(c))) {
             return *c;
         }
     }
@@ -18,7 +18,10 @@ char readChar(BufferedSerial *serialDevice) {
 
 // main() runs in its own thread in the OS
 int main() {
-    char packet[4] = {0};
-    *packet = readChar(&raspi);
-    raspi.write(packet, 4);
+    char packet[3] = {0};
+    while (true) {
+        *packet = readChar(&raspi);
+        raspi.write(&packet, sizeof(packet));
+        printf(packet[3]);
+    }
 }
