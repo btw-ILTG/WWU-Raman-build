1d18969f691d264e2016604d0e0aefe3f3cd4a62
Stefan Hess <speterhess8@gmail.com>
2021-07-08 10:56:10 -0700

    Autosave

--- StefansDev/controlBoard/controlBoardMain.cpp
+++ StefansDev/controlBoard/controlBoardMain.cpp
@@ -2,9 +2,8 @@
 #include <cstdint>
 
 #include "commandCodes.h"
+#include "laserCommands.h"
 #include "pinouts.h"
-
-static DigitalOut myled(LED1);
 
 FileHandle *mbed::mbed_override_console(int fd) {
     return &raspi;
@@ -27,5 +26,52 @@ int main()
         /* parity */ BufferedSerial::None,
         /* stop bit */ 1
     );
+    int packetFlag = 0;
 
+    while (true) {
+        char c;
+
+        c = readChar(&raspi);
+            switch (c) {
+                case packet_start:
+                    raspi.write(&c, 1);
+                    c = readChar(&raspi);
+                    switch (c) {
+                        case packet_flag:
+                            raspi.write(&c, 1);
+                            packetFlag = 1;
+                            while (packetFlag == 1) {
+                                c = readChar(&raspi);
+                                raspi.write(&c, 1);
+                                switch (c) {
+                                    case cmd_laser:
+                                        c = readChar(&raspi);
+                                        raspi.write(&c, 1);
+                                            switch (c) {
+                                                case laser_on:
+                                                   laserPower(on);
+                                                   continue;
+                                                case laser_off:
+                                                    laserPower(off);
+                                                    continue;
+                                                case shutter_open:
+                                                    laserShutter(shutt_open);
+                                                    continue;
+                                                case shutter_close:
+                                                    laserShutter(shutt_close);
+                                                    continue;
+                                                case laser_temp:
+                                                    continue;
+                                                 
+                                            }
+                                        continue;
+                                    case packet_end:
+                                        packetFlag = 0;
+                                        break;
+                                }
+                            }
+                    }
+
+            }
+    }
 }
--- StefansDev/controlBoard/laserCommands.h
+++ StefansDev/controlBoard/laserCommands.h
@@ -3,9 +3,9 @@
 // TODO: Laser TTL 
 
 // laser relay states for on and off:
-enum laser_state {laser_on = 0, laser_off = 1};
+enum laser_state {on = 0, off = 1};
 // servo pulsewidth (in us) for open and close:
-enum laser_shutter_state {shutter_open = 600, shutter_close = 1500}; 
+enum laser_shutter_state {shutt_open = 600, shutt_close = 1500}; 
 
 void laserPower(laser_state setState);
 void laserShutter(laser_shutter_state setState);
