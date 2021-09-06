80d5e543a3134a32922e0e8930249bf5e23c0b6b
Stefan Hess <speterhess8@gmail.com>
2021-09-06 09:11:14 -0700

    beginning to re-write readSerialPacket

--- StefansDev/packetForge/serial-fun/SerialPort.cpp
+++ StefansDev/packetForge/serial-fun/SerialPort.cpp
@@ -130,45 +130,7 @@ int SerialPort::writeSerialRaw(uint8_t* tx_packet, int length) {
 }
 
 int SerialPort::readSerialPacket(vector<uint8_t> &rx_packet) {
-    if (this->serial_port.readable() == 1) {
-        
-        bool packet_started = false;
-        uint8_t read[this->MAX_BUFFER_SIZE];
-        this->serial_port.sync();
-        Timeout read_timeout;
-        this->timedout = false;
-        read_timeout.attach(callback(this, &SerialPort::timeout), TIMEOUT);
-
-        while (this->timedout == false) {
-            memset(read, 0, sizeof(read));
-            this->serial_port.read(read, sizeof(read));
-            for (int i = 0; i < sizeof(read); i++) {
-                if (read[i] == packet_start) {
-                    packet_started = true;
-                    rx_packet.push_back(read[i]);
-                    read_timeout.detach();
-                    goto started_packet;
-                }
-            }
-        }
-
-started_packet:
-
-        while (packet_started == true) {
-            memset(read, 0, sizeof(read));
-            this->serial_port.read(read, sizeof(read));
-            for (int i = 0; i < sizeof(read); i++) {
-                rx_packet.push_back(read[i]);
-                if (read[i] == packet_end) {
-                    return 0;
-                }
-            }
-        }
-        return -2; //TIMEOUT
-    } else {
-        // serial_port does not have a character to read
-        return -1;
-    }
+    
 }
 
 void SerialPort::timeout() {
